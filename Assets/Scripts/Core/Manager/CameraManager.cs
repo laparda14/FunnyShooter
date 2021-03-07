295ea05e32af4f745b9753e1d4080b438bbaa2a8
cetejs <cetejs@gmail.com>
2021-03-07 22:33:00 +0800

    <resources> 更新资源

---
+++ Assets/Scripts/Core/Manager/CameraManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace FunnyShooter.Core {
+    public class InputManager : Singleton<InputManager> {
+
+        public Vector2 MousePosition {
+            get {
+                return Input.mousePosition;
+            }
+        }
+        
+        public float GetAxis(string axisName) {
+            return Input.GetAxis(axisName);
+        }
+
+        public float GetAxisRaw(string axisName) {
+            return Input.GetAxisRaw(axisName);
+        }
+
+        public bool GetButton(string buttonName) {
+            return Input.GetButton(buttonName);
+        }
+
+        public bool GetButtonDown(string buttonName) {
+            return Input.GetButtonDown(buttonName);
+        }
+
+        public bool GetButtonUp(string buttonName) {
+            return Input.GetButtonUp(buttonName);
+        }
+    }
+}
